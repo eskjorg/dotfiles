f5755f4a5c445f22e207b7e7331ba12ac5c22b00
Eskil Jörgensen <eskil.jorgensen@gmail.com>
2019-06-12 14:25:18 +0200

    Force bootstrap checkout

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -4,5 +4,5 @@
 git clone --bare https://github.com/eskjorg/dotfiles.git ~/workspace/dotfiles 
 alias dot='git --git-dir=$HOME/workspace/dotfiles/ --work-tree=$HOME' 
 dot config --local status.showUntrackedFiles no 
-dot checkout 
+dot checkout -f
 dot push -u origin master #set up 'master' to track remote branch 'master'
